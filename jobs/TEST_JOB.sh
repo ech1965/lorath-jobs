de20ac3526ec9bced85baa5299af789b7e2d46bc
Etienne Charlier <etienne@charlier.info>
2017-06-21 20:35:53 +0200

    Fixes

--- jobs/TEST_JOB.sh
+++ jobs/TEST_JOB.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-. /jobs-git/jobs/functions.sh
+. $HOME/jobs/functions.sh
 
+# Job Specific Variables
+
+# Nom du Job
+JOB_NAME=${JOB_NAME:-Undef}
+
+# URL du stockage
+CLOUD_URL=one://_BACKUPS_DO_NOT_TOUCH/DUPLICACY_STORAGE
+
+# Nom du fichiercontenant les token
+TOKEN_FILE_NAME="one-token-lorath-charlier.info.json"
+SRC_DIR=/datahome/$JOB_NAME
+FILTERS_FILE=$HOME/jobs/filters
+
+
+execute $*
